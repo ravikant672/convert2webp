4d557be0a23b67c96c702471be3c4824d3d63555
SqyUser <sqyuser@ravikant.kumar@squareyards.co.in>
2017-10-09 18:25:12 +0530

    jpgpng2webp.sh convert to webp and save in same directory

---
+++ jpgpng2webp.sh
@@ -0,0 +1,45 @@
+#!bin/bash
+## By : Ravikant
+## Date : 7 Oct 2017
+
+input=$1
+
+image2webp () {
+echo $imageName
+## image conversion by google webp lib
+libwebp/bin/cwebp -q 80 $image -o $imageName.webp
+#/usr/local/lib/libwebp/bin/cwebp -q 80 $image -o $output/$imageName.webp
+if [ $? != 0 ];then
+        echo "Error in conversion of images"
+        exit
+fi
+}
+
+if [ -f $input ];then
+	echo "its a file"
+	image=$input
+	imageName=$(echo $image | rev | cut -d'.' -f 2- | rev )
+	image2webp
+fi
+if [ -d $input ];then
+	echo "its a directory"
+	for f in $input/*;do
+		echo $f
+		fileformat=$(file --mime-type -b $f)
+		if [ -d $f ];then
+			echo "its directory"
+			## call recursively for dir
+			sh $0 $f
+			
+		elif [ $fileformat = "image/png" ] || [ $fileformat = "image/jpg" ] || [ $fileformat = "image/jpeg" ];then
+			echo "its a proper file format"
+			echo $f
+			image=$f
+			imageName=$(echo $image | rev | cut -d'.' -f 2- | rev )
+			echo $imageName
+			image2webp
+ 		else
+			echo "Not a valid file"
+		fi
+	done
+fi
